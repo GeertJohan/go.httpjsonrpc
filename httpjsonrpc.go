72e7b2b022ec4cff294aed0ac3fc7a6c03c66eaa
GeertJohan <gjr19912@gmail.com>
2013-01-11 23:28:44 +0100

    Simple HTTP JSON-RPC working.

--- httpjsonrpc.go
+++ httpjsonrpc.go
@@ -1,124 +1,93 @@
 package httpjsonrpc
 
-// This package is modified from the the jsonrpc package (golang.org/pkg/net/rpc/jsonrpc/) to perform http json-rpc.
-
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
-	"net"
-	"net/rpc"
+	"io/ioutil"
+	"net/http"
 	"sync"
 )
 
-type clientCodec struct {
-	dec *json.Decoder
-	enc *json.Encoder
+type Client struct {
+	// remote host url
+	url string
 
-	// http.Request and http.Client to use for communication
-	// TODO: Maybe make an empty post request once in a while to check for notifications?
-	httpReq    *http.Request
+	// http.Client
 	httpClient *http.Client
 
-	// temporary work space
-	req  clientRequest
-	resp clientResponse
+	// basic auth
+	username string
+	password string
 
-	// JSON-RPC responses include the request id but not the request method.
-	// Package rpc expects both.
-	// We save the request method in pending when sending a request
-	// and then look it up by request ID when filling out the rpc Response.
-	mutex   sync.Mutex           // protects pending
-	pending map[uint64]string    // map request id to method name
-	results map[uint64]io.Reader // map request id to result buffer
+	// sequence id
+	id     uint64
+	idLock sync.Mutex
 }
 
-// NewClientCodec returns a new rpc.ClientCodec using JSON-RPC on http.Client.
-func NewClientCodec(httpClient *http.Client, url string) rpc.ClientCodec {
-	bufIn := bytes.Buffer{}
-	bufOut := bytes.Buffer{}
-
-	return &clientCodec{
-		dec:        json.NewDecoder(bufIn),
-		enc:        json.NewEncoder(bufOut),
-		httpReq:    http.NewRequest("post", url, bufOut), // TODO: Is "application/json" required?
-		httpClient: httpClient,
-		pending:    make(map[uint64]string),
+// Create new Client instance
+func NewClient(url string) *Client {
+	return &Client{
+		httpClient: &http.Client{},
+		url:        url,
 	}
 }
 
-type clientRequest struct {
-	Method string         `json:"method"`
-	Params [1]interface{} `json:"params"`
-	Id     uint64         `json:"id"`
+func (c *Client) newId() (id uint64) {
+	c.idLock.Lock()
+	id = c.id
+	c.id++
+	c.idLock.Unlock()
+	return id
 }
 
-func (c *clientCodec) WriteRequest(r *rpc.Request, param interface{}) error {
-	c.mutex.Lock()
-	c.pending[r.Seq] = r.ServiceMethod
-	c.mutex.Unlock()
-	c.req.Method = r.ServiceMethod
-	c.req.Params[0] = param
-	c.req.Id = r.Seq
-	err := c.enc.Encode(&c.req)
+type Request struct {
+	Method string      `json:"method"`
+	Params interface{} `json:"params"`
+	Id     uint64      `json:"id"`
+}
+
+func (c *Client) Call(method string, params interface{}, reply interface{}) error {
+	// create request object
+	reqObj := new(Request)
+	reqObj.Method = method
+	reqObj.Params = params
+	reqObj.Id = c.newId()
+
+	// encode request to buffer
+	bufSend := &bytes.Buffer{}
+	enc := json.NewEncoder(bufSend)
+	err := enc.Encode(reqObj)
 	if err != nil {
 		return err
 	}
-	resp, err := httpClient.Do(c.httpReq)
-}
 
-type clientResponse struct {
-	Id     uint64           `json:"id"`
-	Result *json.RawMessage `json:"result"`
-	Error  interface{}      `json:"error"`
-}
+	// create http request
+	req, err := http.NewRequest("POST", c.url, bufSend)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if len(c.username) > 0 {
 
-func (r *clientResponse) reset() {
-	r.Id = 0
-	r.Result = nil
-	r.Error = nil
-}
+	}
 
-func (c *clientCodec) ReadResponseHeader(r *rpc.Response) error {
-	c.resp.reset()
-	if err := c.dec.Decode(&c.resp); err != nil {
+	// do request
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// unmarshall response
+	bufRecv, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bufRecv, reply)
+	if err != nil {
 		return err
 	}
 
-	c.mutex.Lock()
-	r.ServiceMethod = c.pending[c.resp.Id]
-	delete(c.pending, c.resp.Id)
-	c.mutex.Unlock()
-
-	r.Error = ""
-	r.Seq = c.resp.Id
-	if c.resp.Error != nil {
-		x, ok := c.resp.Error.(string)
-		if !ok {
-			return fmt.Errorf("invalid error %v", c.resp.Error)
-		}
-		if x == "" {
-			x = "unspecified error"
-		}
-		r.Error = x
-	}
+	// done
 	return nil
-}
-
-func (c *clientCodec) ReadResponseBody(x interface{}) error {
-	if x == nil {
-		return nil
-	}
-	return json.Unmarshal(*c.resp.Result, x)
-}
-
-func (c *clientCodec) Close() error {
-	return c.c.Close()
-}
-
-// NewClient returns a new rpc.Client to handle requests to the
-// set of services at the other end of the connection.
-func NewClient(url string) *rpc.Client {
-	cli := &http.Client{}
-	return rpc.NewClientWithCodec(NewClientCodec(cli, url))
 }
